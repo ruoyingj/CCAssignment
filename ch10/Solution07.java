fb5f3ecd402b3ec3c2435bf3418e7a87ac5cda40
Ruoying Jia <carrot@RuoyingdeMBP.wv.cc.cmu.edu>
2015-10-01 12:06:28 -0400

    change 10-7 10-8

--- ch10/Solution07.java
+++ ch10/Solution07.java
@@ -1,6 +1,39 @@
 package ch10;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Solution07 {
+	
+	String filename = "input.txt";
+	void findMissingInt(){
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(new FileReader(filename));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		long numbersets = (long)Integer.MAX_VALUE + 1;
+		byte[] bf = new byte[(int) (numbersets/8)];
+		while(scanner.hasNext()){
+			int temp = scanner.nextInt();
+			int chunkNumber = (int) (numbersets / 8);
+			int bitPosition = (int) (numbersets % 8);
+			bf[chunkNumber] = (byte) (bf[chunkNumber] | (1 << bitPosition));
+			}
+		for(int i = 0; i < bf.length; i++){
+			for(int j = 0; j < 8; j++){
+				int isExit = bf[i] & (1 << j);
+				if(isExit == 0){
+					int value = i * 8 + j;
+					System.out.println(value);
+				}
+			}
+		}
+		
+	}
 	
 
 }
--- ch10/Solution08.java
+++ ch10/Solution08.java
@@ -1,5 +1,38 @@
 package ch10;
 
 public class Solution08 {
+	void findDuplicates(int[] arr){
+		BitVec bv = new BitVec(32000);
+		for(int i = 0; i < arr.length; i++){
+			int temp = arr[i] - 1;
+			if(bv.getValue(temp)){
+				System.out.println(arr[i]);
+			}else{
+				bv.setValue(temp);
+			}
+			
+		}
+		
+	}
+	class 	BitVec{
+		int[] bitvec;
+		public BitVec(int value){
+			int i = value >> 5 + 1;
+			bitvec = new int[i];
+		}
+		void setValue(int value){
+			int chunkNumber = value >> 5;
+			int bitPostion = value & 0x1F;
+			bitvec[chunkNumber] = bitvec[chunkNumber] | 1 << bitPostion ;}
+				
+		boolean getValue(int value){
+			int chunkNumber = value >> 5;
+			int bitPostion = value & 0x1F;
+			boolean isExit = (bitvec[chunkNumber] & 1 << bitPostion) == 1;
+			return isExit;
+		}
+		
+		}
+	}
 
-}
+
