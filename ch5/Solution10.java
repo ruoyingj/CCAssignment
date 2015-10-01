59925de3001ca43313c43fb5afc76ca4b0b695d1
Ruoying Jia <carrot@RuoyingdeMBP.wv.cc.cmu.edu>
2015-10-01 10:59:37 -0400

    ch5

---
+++ ch5/Solution10.java
@@ -0,0 +1,22 @@
+package ch5;
+
+public class Solution10 {
+	
+	
+	class node{
+		public int value;
+		public node left;
+		public node right;
+		public node(int value){
+			this.value = value;
+		}
+		int size_left = 0;
+		public static void track(int value){
+			
+		}
+		public static getRankOfNumber(int target){
+			
+		}
+	}
+
+}
